6c64e514e7a4504bbc8e3b607c489159401fca88
mohammad <137170507+orca0016@users.noreply.github.com>
2024-09-13 16:11:30 +0430

    complete the search header options

--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,6 +1,35 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { AiOutlineCamera, AiOutlineSearch } from "react-icons/ai";
 
 export default function SearchHeaderOptions() {
+  const pathName = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams()
+  const   searchTerm  = searchParams.get("search")
+  const selectTab = (tab) => {
+    router.push(`/search/${tab === "Images" ? "image" : "web"}?searchTerm=${searchTerm}`);
+  };
   return (
-    <div>SearchHeaderOptions</div>
-  )
+    <div className="w-full flex space-x-2 select-none border-b justify-center lg:justify-start text-gray-700 text-sm lg:pl-52">
+      <div
+        onClick={() => selectTab("All")}
+        className={`flex items-center  space-x-1 border-b-4 border-transparent active:text-blue-500  cursor-pointer pb-3 px-2 ${
+          pathName === "/search/web" && "!text-blue-600  !border-blue-600"
+        }`}
+      >
+        <AiOutlineSearch className="text-md" />
+        <p>All</p>
+      </div>
+      <div
+        onClick={() => selectTab("Images")}
+        className={`flex items-center  space-x-1 border-b-4 border-transparent active:text-blue-500  cursor-pointer pb-3 px-2 ${
+          pathName === "/search/image" && "!text-blue-600  !border-blue-600"
+        }`}
+      >
+        <AiOutlineCamera  className="text-md" />
+        <p>Images</p>
+      </div>
+    </div>
+  );
 }
