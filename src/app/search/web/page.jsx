7998dfba591e8f3a3d3a40d6e502c2e0d4f6fb35
mohammad <137170507+orca0016@users.noreply.github.com>
2024-09-14 20:16:34 +0430

    fetch data from google  search api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,16 @@
 
-export default function WebSearchPage() {
+export default async function WebSearchPage({searchParams}) {
+
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY_GOOGLE}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}
+`)
+
+const data =await response.json()
+const results = data.items
   return (
-    <div>WebSearchPage</div>
+    <>
+      {results&& results.map(result=>(
+        <h1>{result.title}</h1>
+      ))}
+    </>
   )
 }
